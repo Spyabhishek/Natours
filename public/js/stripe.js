78035719366ce87c037ec2662f446a207e6768f4
kabhishek2023@gmail.com <kabhishek2023@gmail.com>
2022-02-12 11:21:23 +0530

    Initial Commit

---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+
+export const bookTour = async tourId => {
+    const stripe = Stripe('pk_test_51KQm0iSHWbMsrzJU4mHnKzMP8FnL00uIQGOGcgnJutG6pyfqwLZlZAAO5lnfE5SN0w1JyVm0FTHvt2d67sppwIE300TxUrXEvF');
+     
+    try{
+    // 1) Get Checkout session from API
+    const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`);
+    console.log(session);
+
+    // 2) Create checkout from + chanre credit card
+    await stripe.redirectToCheckout({
+        sessionId: session.data.session.id
+    });
+
+
+    } catch (err) {
+        console.log(err);
+        showAlert('error', err);
+    }
+};
