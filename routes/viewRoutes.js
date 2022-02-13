91531d840b403f360a9f977b19ab073f7d90ffe9
kabhishek2023@gmail.com <kabhishek2023@gmail.com>
2022-02-14 01:10:16 +0530

    fixed bug in signup routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,8 +12,9 @@ router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-
+router.get('/signup', viewsController.getSignupForm);
 router.get('/my-tours', authController.protect, viewsController.getMyTours);
+
 
 router.post(
   '/submit-user-data',
